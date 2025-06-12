4dee12a4fe05c99d896c6c2d6bbd3f368d7f9d70
Adegboyega Adedamola Wilson <wildam001@gmail.com>
2025-06-12 22:10:33 +0100

    *

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import { qrcode } from 'vite-plugin-qrcode'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { qrcode } from "vite-plugin-qrcode";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), qrcode(), tailwindcss()],
   // base:"/cargoconnect"
-})
+  // build: {
+  //   rollupOptions: {
+  //     external: ["react-router-dom"], // Or your specific package
+  //   },
+  // },
+});
